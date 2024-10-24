c4bb10e4f33c1e436ae7831ce24e7a67e7057cfb
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-10-23 22:45:17 -0700

    Initial Commit

---
+++ SimpleCustomerInfoSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleCustomerInfoSystem
+//
+//  Created by Isaac Adeleke on 10/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
