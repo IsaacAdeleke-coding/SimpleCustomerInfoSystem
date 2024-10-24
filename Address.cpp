b451df5bae2658e5c51b8b171a191103f9f3abbf
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-10-23 22:54:11 -0700

    Initial commit: Added Address and Customer structures with constructors. - Implemented Address and Customer structures. - Nested Address inside Customer. - Demonstrated functionality in main.cpp by initializing and printing customer details.

---
+++ Address.cpp
@@ -0,0 +1,8 @@
+//
+//  Address.cpp
+//  SimpleCustomerInfoSystem
+//
+//  Created by Isaac Adeleke on 10/23/24.
+//
+
+#include "Address.hpp"
---
+++ Address.hpp
@@ -0,0 +1,13 @@
+//
+//  Address.hpp
+//  SimpleCustomerInfoSystem
+//
+//  Created by Isaac Adeleke on 10/23/24.
+//
+
+#ifndef Address_hpp
+#define Address_hpp
+
+#include <stdio.h>
+
+#endif /* Address_hpp */
